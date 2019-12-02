7395fec550a77e0393ca5cf2f470f86624058772
Shalennyy <shalennyyv@gmail.com>
2019-12-02 21:53:41 +0200

    second commit

--- applic/app.py
+++ applic/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 @app.route('/')
 def hello_world():
